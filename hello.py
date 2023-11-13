adf30adb0861ddf1ceb1bb2c9181abed43ac1bf4
Hashib Raja <hashibraja23@gmail.com>
2023-11-13 08:34:06 +0530

    added dictionary in python code

--- hello.py
+++ hello.py
@@ -2,4 +2,13 @@ def hello() :
  for i in range(100) :
   print(f"hello {i + 1}")
 
-hello()
+# Creating a dictionary with two details
+user_details = {
+    "name": "John Doe",
+    "age": {
+     "name": "John Doe",
+    "age": 25
+    }
+}
+
+print(user_details["age"]["name"])
