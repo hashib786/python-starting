60ca36d715f31b3c3d432ad5c874154e1566ac7a
Hashib Raja <hashibraja23@gmail.com>
2023-11-13 17:33:22 +0530

    Used turtle module in our project 🐢🐢

---
+++ Turtle/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(200)
+
+myScreen = Screen()
+myScreen.bgcolor("black")
+myScreen.exitonclick()
--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-def hello() :
- for i in range(100) :
-  print(f"hello {i + 1}")
-
 from user_details import user_details
+
+
+def hello():
+    for i in range(100):
+        print(f"hello {i + 1}")
+
 
 print(user_details["age"]["name"])
