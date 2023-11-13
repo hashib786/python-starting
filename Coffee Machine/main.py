8acbdad0eb59c5e6d1850b9ae044ef550d8f9034
Hashib Raja <hashibraja23@gmail.com>
2023-11-13 09:48:02 +0530

    added essential starting code for Coffee Machine

---
+++ Coffee Machine/main.py
@@ -0,0 +1,7 @@
+# 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+# 2. Turn off the Coffee Machine by entering “off” to the prompt.
+# 3. When the user enters “report” to the prompt,
+# 4. Check resources sufficient? --> if not --> “Sorry there is not enough water.”
+# 5. Process coins
+# 6. Check transaction successful? --> if not --> “Sorry that's not enough money. Money refunded.” --> more money will refunded.
+# 7. Make Coffee
