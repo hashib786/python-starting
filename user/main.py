e68a72e7e776235db0b3fa6678a393364457fb9b
Hashib Raja <hashibraja23@gmail.com>
2023-11-16 09:00:05 +0530

    created class in python 🏛️🏛️

---
+++ user/main.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, user_id) -> None:
+        self.id = user_id
+        print("Hello Hashib")
+        print(self.id)
+
+
+user = User(2)
+user1 = User(3)
+user2 = User(4)
+user3 = User(5)
