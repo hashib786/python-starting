553134aa4a8b62fdd3756b83566b402a0d62abf5
Hashib Raja <hashibraja23@gmail.com>
2023-11-21 07:39:10 +0530

    task : implemented rect using turtle and also change color

--- Turtle/main.py
+++ Turtle/main.py
@@ -3,5 +3,17 @@ from turtle import Turtle, Screen
 timmy = Turtle()
 timmy.shape("turtle")
 
+timmy.color("red")
+timmy.fillcolor("green")
+
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+
 myScreen = Screen()
 myScreen.exitonclick()
