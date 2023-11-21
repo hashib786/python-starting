62598538d8ddb6685007b5c1a29e9339c5ab0834
Hashib Raja <hashibraja23@gmail.com>
2023-11-21 08:06:57 +0530

    task : completed task where i draw dotted line - - - - -

--- Turtle/main.py
+++ Turtle/main.py
@@ -3,12 +3,18 @@ from turtle import Turtle, Screen
 timmy = Turtle()
 timmy.shape("turtle")
 
-timmy.color("red")
+timmy.color("black")
 timmy.fillcolor("green")
 
-for _ in range(4):
-    timmy.forward(100)
-    timmy.left(90)
+# for _ in range(4):
+#     timmy.forward(100)
+#     timmy.left(90)
+
+for _ in range(50):
+    timmy.forward(5)
+    timmy.pencolor("white")
+    timmy.forward(5)
+    timmy.pencolor("black")
 
 myScreen = Screen()
 myScreen.exitonclick()
