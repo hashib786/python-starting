d50b6ce43df6e4b5c6182d2eff48a1c85cbc1963
Hashib Raja <hashibraja23@gmail.com>
2023-11-28 07:53:43 +0530

    feat : added paddle and given functionality to up and down ⬆️⬇️

--- pong game/main.py
+++ pong game/main.py
@@ -4,6 +4,7 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Hashib PONG Game")
+screen.tracer(0)
 
 paddle = Turtle()
 paddle.shape("square")
@@ -11,5 +12,24 @@ paddle.color("white")
 paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
 paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
 
 screen.exitonclick()
