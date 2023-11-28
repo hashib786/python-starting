aede6f77426985f17d78a99cac239829c10aac2d
Hashib Raja <hashibraja23@gmail.com>
2023-11-28 07:43:56 +0530

    feat : Added first paddle 🏓🏓

--- pong game/main.py
+++ pong game/main.py
@@ -1,7 +1,15 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
+screen.title("Hashib PONG Game")
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
 
 screen.exitonclick()
