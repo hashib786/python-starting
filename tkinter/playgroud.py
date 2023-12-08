b5d8a19c64c08aeda2df8edd58d568960560e165
Hashib Raja <hashibraja23@gmail.com>
2023-12-08 07:55:59 +0530

    feat : added **kawargs mean you can pass arguments with name and value so when you check in inside def you got dictionary type

--- tkinter/playgroud.py
+++ tkinter/playgroud.py
@@ -6,4 +6,11 @@ def add(*args):
     return sum
 
 
+def calculate(**kawargs):
+    print(type(kawargs))
+    for key, value in kawargs.items():
+        print(key, value)
+
+
 print(add(2, 3, 4, 545454))
+calculate(add=5, wal=5645)
