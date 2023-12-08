3fcf3f544dbfe514d68cdb50b5302ec04a6ccafa
Hashib Raja <hashibraja23@gmail.com>
2023-12-08 08:39:27 +0530

    feat : in window app project added button when button clicked toggle the text

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,12 +1,20 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Hashib First GUI")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="Hashib Raja First GUI",
-                         font=("Arial", 24, "bold"))
+my_label = Label(text="Hashib Raja First GUI",
+                 font=("Arial", 24, "bold"))
 my_label.pack()
+
+
+def button_click():
+    my_label["text"] = "Button Pressed" if my_label["text"] == "Hashib Raja First GUI" else "Hashib Raja First GUI"
+
+
+button = Button(text="Hello", command=button_click)
+button.pack()
 
 
 window.mainloop()
