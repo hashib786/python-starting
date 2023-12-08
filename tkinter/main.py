4d2bd35a40664cd80abf06fe3ddde26748178b99
Hashib Raja <hashibraja23@gmail.com>
2023-12-08 07:36:33 +0530

    feat : in tkinter window i added title, default width, hight and also given label(title)

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,5 +1,12 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title("Hashib First GUI")
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="Hashib Raja First GUI",
+                         font=("Arial", 24, "bold"))
+my_label.pack()
+
 
 window.mainloop()
